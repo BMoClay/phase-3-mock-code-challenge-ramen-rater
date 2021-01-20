02ce1d8d2466d7ad2928a643e0ae3f11c8808984
BMoClay <benmorgancleveland@gmail.com>
2021-01-20 10:39:13 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-// write your code here
+const ramenMenuDiv = document.querySelector("#ramen-menu")
+let ramenDetailDiv = document.querySelector("#ramen-detail")
+
+ramenMenuDiv.addEventListener("click", e => {
+    console.log(e.target)
+    
+})
+
+const renderRamens = (ramen) => {
+    let ramenImg = document.createElement("img")
+    ramenImg.src = ramen.image
+    ramenImg.dataset.id = ramen.id
+  
+    ramenMenuDiv.append(ramenImg)
+}
+
+const getRamens = () => {
+    fetch("http://localhost:3000/ramens")
+    .then(r => r.json())
+    .then((ramenArray) => {
+        ramenArray.forEach(ramen => {
+            renderRamens(ramen)
+        })
+    })
+}
+
+getRamens()
