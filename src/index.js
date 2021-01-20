9c6294e08f985896d71dafc232a68beda5398691
BMoClay <benmorgancleveland@gmail.com>
2021-01-20 12:00:16 -0500

    third commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,18 @@ const ramenMenuDiv = document.querySelector("#ramen-menu")
 let ramenDetailDiv = document.querySelector("#ramen-detail")
 let ramenRatingForm = document.querySelector("#ramen-rating")
 
+ramenRatingForm.addEventListener("submit", e => {
+    e.preventDefault();
+    let ratingId = e.target.dataset.id
+    let newRating = e.target.rating.value
+    let newComment = e.target.comment.value
+    let updateObj = {
+        "rating": newRating,
+        "comment": newComment
+    }
+    updateRamen(updateObj, ratingId)
+})
+
 const renderRamen = (ramen) => {
     ramenDetailDiv.innerHTML = `
     <img class="detail-image" src="${ramen.image}" alt="${ramen.name}" />
@@ -32,6 +44,20 @@ const renderRamens = (ramen) => {
     ramenMenuDiv.append(ramenImg)
 }
 
+const updateRamen = (updateObj, ratingId) => {
+    fetch(`http://localhost:3000/ramens/${ratingId}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(updateObj),
+      })
+    .then(r => r.json())
+    .then((updatedRamenObj) => {
+            renderRamen(updatedRamenObj)
+        })
+}
+
 const getRamen = (id) => {
     fetch(`http://localhost:3000/ramens/${id}`)
     .then(r => r.json())
@@ -39,7 +65,6 @@ const getRamen = (id) => {
             renderRamen(ramenObj)
         })
     }
-
 
 const getRamens = () => {
     fetch("http://localhost:3000/ramens")
